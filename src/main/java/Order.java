f2a4619f52e5c257755435107aa10bb67755234b
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2023-03-10 14:26:21 +0100

    Order and OrderRepo

---
+++ src/main/java/Order.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public class Order {
+    private String orderID;
+    private List<Product> products;
+}
---
+++ src/main/java/OrderRepo.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class OrderRepo {
+
+    private List<Order> orders;
+    public OrderRepo(List<Order> orders){
+        this.orders = orders;
+    }
+    public g
+
+}
